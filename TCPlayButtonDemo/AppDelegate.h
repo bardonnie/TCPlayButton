d67c2b782c53a97ae61cc5269fa8fe5d2470bf8b
xin.chen <xin.chen@wecook.cn>
2016-01-19 16:59:25 +0800

    Initial Commit

---
+++ TCPlayButtonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TCPlayButtonDemo
+//
+//  Created by TonyChan on 16/1/19.
+//  Copyright © 2016年 TonyChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TCPlayButtonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TCPlayButtonDemo
+//
+//  Created by TonyChan on 16/1/19.
+//  Copyright © 2016年 TonyChan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
